00a1c3ca0c14259b8629e55bf1f48b671ca4bad0
ATDoyle <andrew.ti.doyle.com>
2020-02-16 16:39:29 -0500

    exercise 3.1

--- chapter3/src/main/scala/Main.scala
+++ chapter3/src/main/scala/Main.scala
@@ -5,7 +5,7 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-    def sums(ints: List[Int]): Int = ints match {
+    def sum(ints: List[Int]): Int = ints match {
         case Nil => 0
         case Cons(x, xs) => x + sum(xs)
     }
@@ -22,13 +22,19 @@ object List {
 }
 
 // Exercise 3.1
-object Exercise {
-    val x = List(1,2,3,4,5) match {
-        case Cons(x, Cons(2, Cons(4, _))) => x
-        case Nil => 42
-        case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-        case Cons(h, t) => h + sum(t)
-        case _ => 101
+object Exercises {
+    def Exercise31 = {
+        List(1,2,3,4,5) match {
+            case Cons(x, Cons(2, Cons(4, _))) => x
+            case Nil => 42
+            case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+            case Cons(h, t) => h + List.sum(t)
+            case _ => 101
+        }
     }
-    // x = 3
+
+  def main(args: Array[String]): Unit = {
+    val x = Exercise31
+    println(x)
+  }
 }
