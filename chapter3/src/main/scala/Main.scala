ebad0fda05a035e5390622c232f4afed018d2a30
ATDoyle <andrew.ti.doyle.com>
2020-02-18 23:18:07 -0500

    more exercises

--- chapter3/src/main/scala/Main.scala
+++ chapter3/src/main/scala/Main.scala
@@ -19,7 +19,30 @@ object List {
     def apply[A](as: A*): List[A] =
         if (as.isEmpty) Nil
         else Cons(as.head, apply(as.tail: _*))
+    
+    def tail[A](a: List[A]): List[A] = a match { 
+        case Nil => sys.error("tail of empty list")
+        case Cons(_, t) => t
+    }
+
+    def setHead[A](a: List[A], h: A): List[A] = a match {
+        case Nil => List(h)
+        case Cons(_, t) => Cons(h, t)
+    }
+
+    def drop[A](l: List[A], n: Int): List[A] =
+        if (n <= 0) l
+        else l match {
+            case Nil => Nil
+            case Cons(_, t) => drop(t, n-1)
+        }
+    
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+        case Nil => Nil
+        case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+    }
 }
+
 
 // Exercise 3.1
 object Exercises {
@@ -33,8 +56,9 @@ object Exercises {
         }
     }
 
-  def main(args: Array[String]): Unit = {
-    val x = Exercise31
-    println(x)
-  }
+    def main(args: Array[String]): Unit = {
+        val x = List(1,2,3,4,5)
+        println(x)
+        println(List.dropWhile(x, (f: Int) => f%3!=0))
+    }
 }
